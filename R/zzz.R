a1ef1ca6c50b21cedcf3426a2ca028d5774e4527
stenevang <theodor@stenevang.net>
2022-02-03 10:15:18 +0100

    update version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 # run at package attachment by library()
 .onAttach <- function(libname, pkgname){
-    packageStartupMessage("Package sftp v 2.0.10 by Theodor Stenevang Klemming\nUse ?sftp or sftp() for a menu of functions")
+    packageStartupMessage("Package sftp v 2.0.11 by Theodor Stenevang Klemming\nUse ?sftp or sftp() for a menu of functions")
 }
